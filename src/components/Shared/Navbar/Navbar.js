abf7e7ea72681735c6eb05a7b317ffad806bf8ac
Tanjil Ahamed <tanjil113355@gmail.com>
2024-02-12 20:58:18 +0600

    navbar navigation link add complete with responsive

--- src/components/Shared/Navbar/Navbar.js
+++ src/components/Shared/Navbar/Navbar.js
@@ -6,7 +6,12 @@ const Navbar = () => {
 
     const ulLinks = <>
 
-        <li><Link href={'/'}>Home</Link></li>
+        <li><Link href={'/'}>Weddings</Link></li>
+        <li><Link href={'/'}>Parties</Link></li>
+        <li><Link href={'/'}>Corporate Events</Link></li>
+        <li><Link href={'/'}>Birthday Party</Link></li>
+        <li><Link href={'/'}>Photo/ Filmshoot</Link></li>
+        <li><Link href={'/'}>Workshop</Link></li>
 
 
     </>
@@ -14,44 +19,56 @@ const Navbar = () => {
 
 
     return (
-        <div className="navbar">
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+        <div>
+            <div className="navbar">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </div>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow rounded-box w-52">
+                            {
+                                ulLinks
+                            }
+                        </ul>
                     </div>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow rounded-box w-52">
-                        {
-                            ulLinks
-                        }
-                    </ul>
+                    <div className="flex  items-center">
+                        <Image className="w-10" src={'https://i.ibb.co/Sy34qfd/Logo.png'} width={500} height={500} alt="Website logo" />
+                        <h2 className="text-xl font-bold text-[#040404]">BookPartyPlace</h2>
+                    </div>
                 </div>
-                <div className="flex  items-center">
-                    <Image className="w-10" src={'https://i.ibb.co/Sy34qfd/Logo.png'} width={500} height={500} alt="Website logo" />
-                    <h2 className="text-xl font-bold text-[#040404]">BookPartyPlace</h2>
-                </div>
-            </div>
-            {/* <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                    {
-                        ulLinks
-                    }
-                </ul>
-            </div> */}
-            <div className="navbar-end flex items-center gap-10">
-                <div className="text-[#535552] text-lg md:flex items-center gap-8 hidden">
-                    <h2>Are u a Vendor ?</h2>
-                    <h2>Blog</h2>
-                </div>
-                <div className="flex items-center gap-5">
-                    <FaRegHeart className="text-4xl text-[#BCBFBA] hidden md:flex"  />
-                    <div className="avatar">
-                        <div className="w-10 rounded-full">
-                            <Image src={'https://i.ibb.co/k3twN21/7c81e16cc1f60c3091c8fd5e7d764f3b.jpg'} alt="profile image" width={500} height={500} />
+                <div className="navbar-end flex items-center gap-10">
+                    <div className="text-[#535552] text-lg md:flex items-center gap-8 hidden">
+                        <h2>Are u a Vendor ?</h2>
+                        <h2>Blog</h2>
+                    </div>
+                    <div className="flex items-center gap-5">
+                        <FaRegHeart className="text-4xl text-[#BCBFBA] hidden md:flex" />
+                        <div className="avatar">
+                            <div className="w-10 rounded-full">
+                                <Image src={'https://i.ibb.co/k3twN21/7c81e16cc1f60c3091c8fd5e7d764f3b.jpg'} alt="profile image" width={500} height={500} />
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+            {/* navigation links */}
+
+            <div>
+                <div className="mt-2">
+                    <hr className="w-full text-[#535552] hidden md:flex" />
+                    
+                    {/* ul links */}
+                    <ul className="menu menu-horizontal px-1 justify-center md:flex font-semibold  text-xl hidden">
+                        {
+                            ulLinks
+                        }
+                    </ul>
+
+                    <hr className="w-full text-[#535552 ]  hidden md:flex " />
+                </div>
+            </div>
+
         </div>
     );
 };
