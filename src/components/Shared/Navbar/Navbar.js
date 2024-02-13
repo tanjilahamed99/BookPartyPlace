ffd30d2a6ae79bdb318c26fc41546afc0fe3f151
Tanjil Ahamed <tanjil113355@gmail.com>
2024-02-13 12:50:30 +0600

    header section complete with responsive

--- src/components/Shared/Navbar/Navbar.js
+++ src/components/Shared/Navbar/Navbar.js
@@ -32,7 +32,7 @@ const Navbar = () => {
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </div>
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow rounded-box w-52">
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content bg-primary-color mt-3 z-[1] p-2 shadow rounded-box w-52">
                             {
                                 ulLinks
                             }
@@ -60,9 +60,9 @@ const Navbar = () => {
             </div>
 
             {/* navigation links */}
-            <div>
-                <div className="mt-2">
-                    <hr className="w-full text-primary-text-gray hidden lg:flex mb-5" />
+            <div className="lg:flex hidden justify-center mx-auto w-full">
+                <div className="mt-2 ">
+                    <hr className="w-full text-primary-text-gray lg:flex mb-5" />
 
                     {/* ul links */}
                     <Dropdown />
