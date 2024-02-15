1c7a1d36c6b2bcf5743a1a3e8172305e32f17db0
Tanjil Ahamed <tanjil113355@gmail.com>
2024-02-15 11:39:06 +0600

    footer completed with responsive

--- src/components/Shared/Footer/Footer.js
+++ src/components/Shared/Footer/Footer.js
@@ -1,11 +1,14 @@
 import Image from "next/image";
-import { GoSearch } from "react-icons/go";
 import SectionButton from "../SectionButton/SectionButton";
+import Link from "next/link";
+import { FaFacebookF, FaTwitter, FaLinkedinIn } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa6";
+import { GrYoutube } from "react-icons/gr";
 
 const Footer = () => {
     return (
-        <footer className="footer my-20 grid grid-cols-3">
-            <div>
+        <footer className="footer mt-20 flex flex-col lg:flex-row xl:gap-20 lg:gap-5 lg:items-center px-5">
+            <div className="lg:w-1/3 mx-auto w-full">
                 <div className="flex  items-center">
                     <Image className="w-10" src={'https://i.ibb.co/Sy34qfd/Logo.png'} width={500} height={500} alt="Website logo" />
                     <h2 className="text-xl font-bold text-[#040404]">BookPartyPlace</h2>
@@ -13,42 +16,29 @@ const Footer = () => {
                 <h2>
                     Lorem ipsum dolor sit amet consectetur dolol drakgonil adipiscing elit aliquam mauris
                 </h2>
-                <div className="lg:w-2/3  mt-5 relative w-[80%]">
+                <div className="lg:w-[80%]  mt-5 relative w-[90%]">
                     <input type="text" placeholder="Your email" className="input input-bordered md:py-4 w-full" />
-                    <div className="absolute -right-10 top-0">
+                    <div className="absolute -right-10 top-[1.8px]">
                         <SectionButton text={'Subscribe'} />
                     </div>
                 </div>
             </div>
-            <div>
-                <div className="flex  items-center">
-                    <Image className="w-10" src={'https://i.ibb.co/Sy34qfd/Logo.png'} width={500} height={500} alt="Website logo" />
-                    <h2 className="text-xl font-bold text-[#040404]">BookPartyPlace</h2>
-                </div>
-                <h2>
-                    Lorem ipsum dolor sit amet consectetur dolol drakgonil adipiscing elit aliquam mauris
-                </h2>
-                <div className="lg:w-2/3  mt-5 relative w-[80%]">
-                    <input type="text" placeholder="Your email" className="input input-bordered md:py-4 w-full" />
-                    <div className="absolute -right-10 top-0">
-                        <SectionButton text={'Subscribe'} />
-                    </div>
-                </div>
+            <div className="text-[#040404] font-semibold md:text-xl  grid grid-cols-3 gap-5 lg:w-1/3 mx-auto w-full">
+                <Link href={'/'}><h1>Contact</h1></Link>
+                <Link href={'/'}><h1>About</h1></Link>
+                <Link href={'/'}><h1>Booking</h1></Link>
+                <Link href={'/'}><h1>Our Team</h1></Link>
+                <Link href={'/'}><h1>Offers</h1></Link>
             </div>
-            <div>
-                <div className="flex  items-center">
-                    <Image className="w-10" src={'https://i.ibb.co/Sy34qfd/Logo.png'} width={500} height={500} alt="Website logo" />
-                    <h2 className="text-xl font-bold text-[#040404]">BookPartyPlace</h2>
+            <div className=" lg:w-1/3 mx-auto w-full">
+                <div className="flex gap-6 items-center">
+                    <FaFacebookF className="bg-[#61687233] text-4xl p-2 rounded-xl"/>
+                    <FaTwitter className="bg-[#61687233] text-4xl p-2 rounded-xl"/>
+                    <FaInstagram className="bg-[#61687233] text-4xl p-2 rounded-xl"/>
+                    <FaLinkedinIn className="bg-[#61687233] text-4xl p-2 rounded-xl"/>
+                    <GrYoutube className="bg-[#61687233] text-4xl p-2 rounded-xl"/>
                 </div>
-                <h2>
-                    Lorem ipsum dolor sit amet consectetur dolol drakgonil adipiscing elit aliquam mauris
-                </h2>
-                <div className="lg:w-2/3  mt-5 relative w-[80%]">
-                    <input type="text" placeholder="Your email" className="input input-bordered md:py-4 w-full" />
-                    <div className="absolute -right-10 top-0">
-                        <SectionButton text={'Subscribe'} />
-                    </div>
-                </div>
+                <p className="text-[#535552] mt-3">2024 BookPartyPlace. All Rights Reserved</p>
             </div>
         </footer>
     );
