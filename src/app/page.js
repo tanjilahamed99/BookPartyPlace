600b58302d86791e8802e8900135f24ba9f221a4
Tanjil Ahamed <tanjil113355@gmail.com>
2024-02-22 22:18:47 +0600

    new code added

--- src/app/page.js
+++ src/app/page.js
@@ -39,7 +39,7 @@ const Home = async () => {
       <UniqueVenues data={data} />
 
       {/* Testimonial */}
-      <Testimonials />
+      {/* <Testimonials /> */}
 
       {/* work commands */}
       <WorkCommand />
